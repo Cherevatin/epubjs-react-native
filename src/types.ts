76a9459119934d897ab2a34faa9f7e43510ae794
Cherevatin <cherevatin3356@gmail.com>
2025-03-07 11:21:41 +0200

    exported EventTypeMap

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,53 @@
 import { DimensionValue, ViewStyle } from 'react-native';
 import { SourceType } from './utils/enums/source-type.enum';
+import { EventType } from './utils/enums/event-type.enum';
 import { WebViewCustomMenuItems } from 'react-native-webview/lib/WebViewTypes';
+
+export type EventTypeMap = {
+  [EventType.OnStarted]: undefined;
+  [EventType.OnReady]: {
+    totalLocations: number;
+    currentLocation: Location;
+    progress: number;
+  };
+  [EventType.OnDisplayError]: { reason: string };
+  [EventType.OnResized]: { layout: any };
+  [EventType.OnLocationChange]: {
+    totalLocations: number;
+    currentLocation: Location;
+    progress: number;
+    currentSection: Section | null;
+  };
+  [EventType.OnSearch]: { results: SearchResult[]; totalResults: number };
+  [EventType.OnLocationsReady]: {
+    epubKey: string;
+    locations: ePubCfi[];
+  };
+  [EventType.OnSelected]: { text: string; cfiRange: ePubCfi };
+  [EventType.OnOrientationChange]: Orientation;
+  [EventType.OnBeginning]: undefined;
+  [EventType.OnFinish]: undefined;
+  [EventType.OnLayout]: { layout: any };
+  [EventType.OnNavigationLoaded]: { toc: Toc; landmarks: Landmark[] };
+  [EventType.OnAddAnnotation]: Annotation;
+  [EventType.OnPressAnnotation]: Annotation;
+  [EventType.OnChangeAnnotations]: Annotation[];
+  [EventType.OnPressFootnote]: Footnote;
+  [EventType.OnAddBookmark]: Bookmark;
+  [EventType.OnChangeBookmarks]: Bookmark[];
+  [EventType.OnRemoveBookmark]: Bookmark;
+  [EventType.OnUpdateBookmark]: Bookmark;
+  [EventType.OnRemoveBookmark]: Bookmark;
+  [EventType.OnSwipeLeft]: undefined;
+  [EventType.OnSwipeRight]: undefined;
+  [EventType.OnSwipeUp]: undefined;
+  [EventType.OnSwipeDown]: undefined;
+  [EventType.OnPress]: undefined;
+  [EventType.OnSingleTap]: undefined;
+  [EventType.OnDoubleTap]: undefined;
+  [EventType.OnLongPress]: undefined;
+  [EventType.OnCustomMenuSelection]: CustomMenuItemData;
+};
 
 export type PaginateOptions = {
   keepScrollOffset?: boolean;
